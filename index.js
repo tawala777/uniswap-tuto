b575b500f55fc121a7876b880a262d10538a4743
Andrew <ack@heytom.io>
2022-09-13 22:41:44 +0200

    coming ethers

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-﻿const {ChainId, Fetcher,WETH, Route, Trade, TokenAmount, TradeType} = require('@uniswap/sdk')
+﻿const {ChainId, Fetcher,WETH, Route, Trade, TokenAmount, TradeType, Percent} = require('@uniswap/sdk')
+const ethers = require('ethers')
 const chainId = ChainId.MAINNET
 const tokenAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F' //Dai Token address
+
 const init = async () => {
     const dai = await Fetcher.fetchTokenData(chainId, tokenAddress)
-    // console.log('dai::::::::::::',dai)
-
     const weth = WETH[chainId]
-    // console.log('weth::::::::::::',weth)
-
     const pair = await Fetcher.fetchPairData(dai,weth)
-    // console.log('pair::::::::::::',pair)
-
     const route = new Route([pair],weth)
-    // console.log('route::::::::::::',route.midPrice)
-
     console.log('weth in dai',route.midPrice.toSignificant(6))
-    // console.log('dai in weth',route.midPrice.invert().toSignificant(6))
-
-    const trade = new Trade(route, new TokenAmount(weth, '1000'),TradeType.EXACT_INPUT)
+    const trade = new Trade(route, new TokenAmount(weth, '100000000000000000'),TradeType.EXACT_INPUT)
+    console.log('inputAmount',trade.inputAmount.toSignificant(6))
     console.log('execPrice',trade.executionPrice.toSignificant(6))
     console.log('next MidPrice',trade.nextMidPrice.toSignificant(6))
+    const slippageTolerance = new Percent('50','10000') // 50 bips .. 1 bip = 1/100 de %   
+    const amountOutMin = trade.minimumAmountOut(slippageTolerance).raw   
+    console.log('amountOutMin:::::::::',amountOutMin)
+    const path = [weth.address,dai.address]
+    const to = ''
+    const deadLine = Math.floor(Date.now() / 1000) + 60 * 20
+    const value = trade.inputAmount.raw
+    // const provider = ethers.getDefaultProvider('mainnet',{
+    //     infura: ''
+    // })
 }
 init()
